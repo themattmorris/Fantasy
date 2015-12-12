d6309646a2bdf3ec7b767b72fd9bb943fefae25d
Brtt Stl <brttstl@gmail.com>
2015-12-12 15:04:22 -0800

    updated
    updated output

--- 4.lineup_optimization/optimal_lineup.r
+++ 4.lineup_optimization/optimal_lineup.r
@@ -1,9 +1,12 @@
-#### load packages and fix functions ####
+#### install & load packages and fix functions ####
+install.packages("Rglpk")
+install.packages("dplyr")
+
 data.packages <- c('dplyr', 'Rglpk')
 lapply(data.packages, library, character.only = T)
 
 #### IMPORTANT: specify final output path here ####
-output.path = "/Users/brett/GitHub/proj-fantasy/5._final_optimized_lineup/optimal_lineup.csv"
+output.path = "/Users/brett/GitHub/proj-fantasy/5.final_optimized_lineup/optimal_lineup.csv"
 
 #### import output from player_projections_&_fanduel_merge.py ####
 players <- read.csv("https://raw.githubusercontent.com/brttstl/proj-fantasy/master/data/player_pool.csv", header = TRUE, ",", skipNul = FALSE, stringsAsFactors = FALSE)
