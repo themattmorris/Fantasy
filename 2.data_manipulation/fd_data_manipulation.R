234e46675f2b49b71a5f93ed680a7e44e9c5b07a
Brtt Stl <brttstl@gmail.com>
2015-12-12 14:37:20 -0800

    updated
    revised to output correct format and install packages

--- 2.data_manipulation/fd_data_manipulation.R
+++ 2.data_manipulation/fd_data_manipulation.R
@@ -1,12 +1,21 @@
-#### load packages and fix functions ####
+#### install & load packages and fix functions ####
+## install package function
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+## packages to install and load
 data.packages <- c('dplyr', 'ggplot2', 'RColorBrewer', 'RCurl', 'ggthemes', 'ggExtra', 'gridExtra', 'scales', 'tidyr', 'MASS', 'devtools', 'lubridate', 'mosaic')
+ipak(data.packages)
 lapply(data.packages, library, character.only = T)
 
 ## redefine select from dplyr because MASS package causes issues
 select <- dplyr::select
 
 #### IMPORTANT: specify final output path here ####
-output.path = "/Users/brett/GitHub/proj-fantasy/data/fanduel_2015_season_summary.csv"
+output.path = "/Users/brett/GitHub/proj-fantasy/data/fanduel_season_data.csv"
 lookup.table.path = "/Users/brett/GitHub/proj-fantasy/data/lookup_table.csv"
 
 #### bring in salary data from fanduel ####
@@ -16,12 +25,12 @@ salary <- tbl_df(read.csv("https://raw.githubusercontent.com/brttstl/proj-fantas
 #### clean up ####
 ## remove gid column and filter out def for name format reshape remove salaries that are 0 as these players weren't available
 no.def <- salary %>%
-  select(Season = Year, Week, Name, Position = Pos, Team, Home.Away = h.a, Opponent = Oppt, FanDuel.pts = FD.points, FanDuel.sal = FD.salary) %>% 
-  filter(Position %in% c("QB", "RB", "WR", "TE", "PK"), FanDuel.sal >= 1)
+  select(Week, Year, GID, Name, Pos, Team, "h/a" = h.a, Oppt, "FD points" = FD.points, Salary = FD.salary) %>% 
+  filter(Pos %in% c("QB", "RB", "WR", "TE", "PK"), Salary >= 1)
 
 def <- salary %>%
-  select(Season = Year, Week, Name, Position = Pos, Team, Home.Away = h.a, Opponent = Oppt, FanDuel.pts = FD.points, FanDuel.sal = FD.salary) %>% 
-  filter(Position == "Def", FanDuel.sal >= 1)
+  select(Week, Year, GID, Name, Pos, Team, "h/a" = h.a, Oppt, "FD points" = FD.points, Salary = FD.salary) %>% 
+  filter(Pos == "Def", Salary >= 1)
 
 ## split names by comma
 split <- no.def %>%
@@ -36,18 +45,28 @@ clean <- rbind(reorg, def)
 ## lookup tables
 library(devtools, quietly=TRUE)
 source_gist("https://gist.github.com/dfalster/5589956")
-allowedVars <- c("Name", "Position", "Home.Away")
+allowedVars <- c("Name", "Pos")
 fully.cleaned <- addNewData(lookup.table.path, clean, allowedVars)
 
 ## format columns to match profootball reference export
-fully.cleaned$Season <- as.integer(fully.cleaned$Season)
+fully.cleaned$Year <- as.integer(fully.cleaned$Year)
 fully.cleaned$Week <- as.integer(fully.cleaned$Week)
-fully.cleaned$Position <- factor(fully.cleaned$Position, levels = c("QB", "RB", "WR", "TE", "K", "DEF"))
-fully.cleaned$Team <- as.factor(toupper(fully.cleaned$Team))
-fully.cleaned$Opponent <- as.factor(toupper(fully.cleaned$Opponent))
-fully.cleaned$Home.Away <- as.factor(fully.cleaned$Home.Away)
-fully.cleaned$FanDuel.pts <- as.double(fully.cleaned$FanDuel.pts)
-fully.cleaned$FanDuel.sal <- as.double(fully.cleaned$FanDuel.sal)
+fully.cleaned$Pos <- factor(fully.cleaned$Pos, levels = c("QB", "RB", "WR", "TE", "K", "DEF"))
+fully.cleaned$`FD points` <- as.double(fully.cleaned$`FD points`)
+fully.cleaned$Salary <- as.double(fully.cleaned$Salary)
 
-## save cleaned fanduel output
-write.csv(fully.cleaned, output.path)
+## create conditional location column
+final <- fully.cleaned %>%
+  mutate(Location = ifelse(`h/a` == "h", Team, Oppt))
+
+## rearrange rows
+final <- final[,c("Week", "Year", "GID", "Name", "Pos", "Team", "Location", "h/a", "Oppt", "FD points", "Salary")]
+
+## finish proper formatting
+final$Team <- as.factor(toupper(final$Team))
+final$Oppt <- as.factor(toupper(final$Oppt))
+final$`h/a` <- as.factor(final$`h/a`)
+final$Location <- as.factor(toupper(final$Location))
+
+#### save cleaned fanduel output ####
+write.csv(final, output.path, row.names = FALSE)
